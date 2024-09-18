5adf3a9f1e9c503b9aa239cf4df6ecf13dd9d4a4
lebrancconvas <lebrancconvas@gmail.com>
2024-09-19 02:14:13 +0700

    💡 chore: add comment on run script

--- arm64/run.sh
+++ arm64/run.sh
@@ -1,5 +1,15 @@
+# Assembly 
 as -o "$1".o "$1".s &&
+
+
+# Linking
 ld -o "$1" "$1".o -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64 &&
+
+
+# Run
 ./"$1" &&
+
+
+# Clean
 rm "$1" "$1".o
 
